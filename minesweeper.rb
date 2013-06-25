71160f3da19ce39cdec5e83c32fea6be753d78d0
Anthony Mangano <amangano86@gmail.com>
2013-06-24 22:04:50 -0400

    code review

--- minesweeper.rb
+++ minesweeper.rb
@@ -2,10 +2,20 @@ require 'yaml'
 require 'json'
 class Minesweeper
 
+	#REV: Your game has a bug somewhere. When I run it I get an error that says
+	#the process_move method has the wrong number of arguments.
+  
+  #Your code may be easier to manage if you break it up into separate classes.
+  #You could make a main game_class that controls game operations and a board
+  #class that keeps track of the board. 
+
   attr_reader :board, :shown, :flags
 
   def initialize(n = 9)
     @num_bombs = (n == 9 ? 10 : 40)
+		
+		#REV: You should pass n to generate_board so that you can generate
+		#the 16X16 board if the user initializes the game with n = 16.
     @board = generate_board(9)
     @flags = []
     @shown = []
@@ -16,6 +26,11 @@ class Minesweeper
   def play_game
     timer
     until has_won?
+			#REV: Maybe move all of the puts statements and ui stuff to a separate 
+			#method like 'get_move' or something. Try to keep methods focused on
+			#a single task. Also, you can wrap the ui stuff in a loop and break
+			#out of it after you verify that the input is valid. Otherwise, 
+			#your game might crash if the input isn't formatted right.
       print_board
       puts "Enter coordinates separated by a comma (e.g. 0,0):"
       coordinates = gets.chomp.split(",").map!{|x| x.to_i}
@@ -29,6 +44,9 @@ class Minesweeper
           puts "You Lose. Your time was #{time_diff}"
           return nil
         end
+  			#REV: I think that you should use elsif instead of just else. The
+        #current program would call mark_flag for anything typed in other than
+        #save or r
       else
         mark_flag(*coordinates)
       end
@@ -38,6 +56,7 @@ class Minesweeper
   end
 
   def has_won?
+		#REV: I'm not sure what you're doing here. Maybe add a comment.
     @shown.length == @board.length ** 2 - @num_bombs
   end
 
@@ -165,6 +184,9 @@ class Minesweeper
     timer
     puts "What would you like to save it as?"
     file_name = gets.chomp
+		
+		#REV: You can condense your file writing code into its own block like this
+		#File.open(filename, 'w'){|f| f.puts self.to_yaml }.
     file = File.open("#{file_name}.yml",'w')
     file.puts self.to_yaml
     file.close
@@ -173,6 +195,8 @@ class Minesweeper
 
   # load game from YAML
   def self.load_game(file_name)
+		#REV: You can condense this into one line like this
+		#YAML::load(File.read(filename)).
     f = File.open("#{file_name}", 'r')
     YAML::load(f)
   end
@@ -213,12 +237,17 @@ class Minesweeper
     f = File.open("high_scores.json", "w")
     f.puts high_score_hash.to_json
   end
-end
 =end
+#REV: I moved the end of your comment up. It was covering the end statement
+#for your class and causing an error.	
+end
+
 
 # Menu function
 
 def main_menu
+	#REV: I don't think you need to declare ans here, since you only use it 
+  #inside of the loop.
   ans = ''
   loop do
     puts "[1] New 9x9 Game"
@@ -231,9 +260,17 @@ def main_menu
     if ans == 3
       puts "Enter your saved game filename (e.g. mygame.yml)"
       return Minesweeper.load_game(gets.chomp)
+			
+		#REV: What if ans == 4? You should probably just have an else here, since
+		#you probably want to keep looping around until you get an answer between
+		#1 and 3.	Also, you should probably reprompt them to select from 1-3, not 
+    #1-5
     elsif ans != 4
       puts "Invalid Option. Please select [1-5]."
     end
+		#REV: I'm not sure what the while statement is for but I think that you
+		#don't need it. You should keep looping until you return as a result of 
+    #the user entering 1, 2, or 3.	
   end while ans != 4
 end
 
